d1a521c7c68eb4b6bfceb884ea46938e6a2cb8cf
vignesh070198 <nic.chennai.irad@gmail.com>
2025-12-02 22:17:57 +0530

    gh

--- 02-12-202/callback.js
+++ 02-12-202/callback.js
@@ -1,6 +1,55 @@
 
-let a = 10;
+let a = 9;
+
+let b = 8;
 
 function oddeven(a) {
-    return (a % 2)
+    if (a % 2 == 0) {
+        return "even";
+    }
+    else {
+        return "odd";
+    }
 }
+
+console.log(oddeven(b));
+
+let even = oddeven(8);
+console.log(even);
+
+let x = (y) => y % 2 == 0 ? "even" : "odd";
+
+console.log("x" + x(9));
+
+
+function history(name, age, callback) {
+    return callback(name, age);
+}
+
+
+let person1 = (name, age) => {
+    console.log("My name is " + name + " and I am " + age + " years old.");
+}
+
+
+console.log(history("Alice", 30, person1));
+
+let person2 = function (name, age) {
+    console.log("My name is " + name + " and I am " + age + " years old.");
+}
+
+console.log(history("Bob", 25, person2));
+
+
+
+function message(name, ab) {
+    console.log("Inside message function");
+    const gh = "hello" + name;
+    ab(gh);
+}
+
+
+message("Charlie", function (gh) {
+    console.log("Callback function called with message: " + gh);
+});
+
