9211c03f39a028ece87aecd6b81b2f2f83bed3ef
SKarthick <karthickhari@gmail.com>
2025-12-05 18:48:10 +0530

    new changes

--- 04-12-2024/async.js
+++ 04-12-2024/async.js
@@ -1,9 +1,9 @@
-
-function loadData() {
+function async() {
     document.getElementById("aysnc").innerHTML = "loading...";
 
     setTimeout(() => {
-        document.getElementById("aysnc").innerHTML = "i'm loaded"
+        //document.getElementById("aysnc").innerHTML = "i'm loaded"
+        document.getElementsByClassName("mza-wrapper")[0].innerHTML = "i'm loaded"
 
     }, 1800);
 
